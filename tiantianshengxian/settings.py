07f39f0beb64f011b3aaedbf5358ef3c0a0e3017
lianght <619713217@qq.com>
2017-08-24 20:56:58 +0800

    修改setting

--- tiantianshengxian/settings.py
+++ tiantianshengxian/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tiantianshengxian.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tiantianshengxian.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tiantian',
+        'USER': 'lianghaitao',
+        'PASSWORD': '123456',
+        'HOST': '192.168.1.29',
+        "PORT": '3306',
     }
 }
 
