930508d67923e721b67f321183a312d6d44e3032
lianght <619713217@qq.com>
2017-08-25 17:31:58 +0800

    最新更改

---
+++ df_user/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^register$', views.register),
+    url(r'^register_handle$', views.register_handle),
+    url(r'^$', views.index),
+    url(r'^login$', views.login),
+    url(r'^register_exist', views.register_exist),
+    url(r'^login_handle$', views.login_handle),
+    url(r'^logout$', views.logout),
+    url(r'^info$', views.user_info),
+    url(r'^order$', views.user_order),
+    url(r'^site$', views.user_site),
+    url(r'^areas', views.areas),
+]
